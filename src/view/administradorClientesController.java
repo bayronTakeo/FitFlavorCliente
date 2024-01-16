20d1a8866a67aee5200239aa7bb0a57fe5d8a1cc
bayron <bayrontakeoch21@gmail.com>
2024-01-16 21:35:26 +0100

    feat(administradorClientes):Añado vista y un controlador inicial para administradorClientes

---
+++ src/view/administradorClientesController.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+/**
+ *
+ * @author bayro
+ */
+public class administradorClientesController {
+
+    private Stage stage;
+
+    private static final Logger LOGGER = Logger.getLogger("SignInController.class");
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void initStage(Parent root) {
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+
+        stage.setTitle("Administracion clientes");
+        stage.setResizable(false);
+        stage.setOnCloseRequest(this::handleExitAction);
+
+        stage.show();
+        LOGGER.info("SingIn window initialized");
+
+    }
+
+    private void handleExitAction(WindowEvent event) {
+        Alert a = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to exit? This will close the app.");
+        a.showAndWait();
+        try {
+            if (a.getResult().equals(ButtonType.CANCEL)) {
+                event.consume();
+            } else {
+                Platform.exit();
+            }
+        } catch (Exception e) {
+            String msg = "Error closing the app: " + e.getMessage();
+            Alert alert = new Alert(Alert.AlertType.ERROR, msg);
+            alert.show();
+            LOGGER.log(Level.SEVERE, msg);
+        }
+    }
+}
