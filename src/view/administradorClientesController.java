e155202f687c5823ef94e6ca03e133bea676af9c
bayron <bayrontakeoch21@gmail.com>
2024-01-16 21:46:16 +0100

    feat(administradorClientes):Añado atributos del fxml para empezar a trabajar.

--- src/view/administradorClientesController.java
+++ src/view/administradorClientesController.java
@@ -8,12 +8,18 @@ package view;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import objects.Cliente;
 
 /**
  *
@@ -23,7 +29,27 @@ public class administradorClientesController {
 
     private Stage stage;
 
-    private static final Logger LOGGER = Logger.getLogger("SignInController.class");
+    private static final Logger LOGGER = Logger.getLogger("administradorClientesController.class");
+    @FXML
+    private ContextMenu menuTabla;
+    @FXML
+    private TableView tablaUsuarios;
+    @FXML
+    private TableColumn<Cliente, String> columnaEmail;
+    @FXML
+    private TableColumn<Cliente, String> columnaNombre;
+    @FXML
+    private TableColumn<Cliente, String> columnafecha;
+    @FXML
+    private TableColumn<Cliente, String> columnaTelefono;
+    @FXML
+    private TableColumn<Cliente, String> columnaDireccion;
+    @FXML
+    private TableColumn<Cliente, String> columnaCodPostal;
+    @FXML
+    private TableColumn<Cliente, String> columnaContrania;
+
+    private ObservableList<Cliente> informacionClientes;
 
     public void setStage(Stage stage) {
         this.stage = stage;
